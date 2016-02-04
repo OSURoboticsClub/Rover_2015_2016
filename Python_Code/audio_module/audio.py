cad7c55a3e7350c5e3471b2acf14d7ddaaa13ed8
River Hendriksen <RiverHendriksen@Rivers-MacBook-Pro-2.local>
2016-02-04 15:16:04 -0800

    adding audio module, just import the file and choose a number value for the music you want to play. audio can be added

---
+++ Python_Code/audio_module/audio.py
@@ -0,0 +1,31 @@
+#audio file player that can be imported, import file can choose from a list what audio file to play
+#music choices (can be added to)
+#1. Star wars theme
+#2. Claire De Lune
+
+
+#allows us to use audio files of our choice and load it into the program
+
+import pyglet
+
+pyglet.options['audio'] = ('openal', 'silent')  #chooses the driver for audio, if the audio driver is not selected it will not play anything
+                                                #mac uses open al, but ubuntu can get this using their package manager, or switch to Pulse which is built in
+
+audio = ['starwars.wav','clairedelune.wav'] #string list of audio files names
+
+def exit_callback(dt):  #function that will be called to close program
+    pyglet.app.exit()
+
+
+def song(x):   #main function that can be used to call different songs to play
+    path = '/whatever/linux_directory/there_is/pythonstuff/music/' + audio[x]   #this path must be changed 
+    sound = pyglet.media.load(path, streaming=False)    #loads song into variable with the correct path 
+    sound.play()
+    pyglet.clock.schedule_once(exit_callback , sound.duration) #will enact exit_callback when the sound is over    
+    pyglet.app.run() #runs song
+
+#code cannot progress until the song is finished playing because the app is running 
+#need to thread
+
+
+
