c5e78ad3de67001a5c23c5e07372981b45713b1f
reindels <reindels@oregonstate.edu>
2016-02-29 19:41:28 -0800

    Improved the modularity of Color_Filter.py.

--- Obj Rec Testing/Color_Filter.py
+++ Obj Rec Testing/Color_Filter.py
@@ -53,6 +53,75 @@ def filters():
     uppers[purple] = purple_upper
     return {"Lower":lowers, "Upper":uppers, "Filters":color_filters}
 
+def filter_colors(frame=None, show_images=False, verbose=False):
+    if frame is None:
+        return False
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    fd = filters()
+    lowers = fd["Lower"]
+    uppers = fd["Upper"]
+    color_filters = fd["Filters"]
+
+    masks = {}
+    results = {}
+    medians = {}
+    smoothed = {}
+    kernel = np.ones((15,15), np.float32) / 225
+
+    for color in color_filters:
+        lower_color = np.array(lowers[color])
+        upper_color = np.array(uppers[color])
+        masks[color] = cv2.inRange(hsv, lower_color, upper_color)
+        results[color] = cv2.bitwise_and(frame, frame, mask=masks[color])
+        medians[color] = cv2.medianBlur(results[color], 15)
+        smoothed[color] = cv2.filter2D(results[color], -1, kernel)
+
+    edges = cv2.Canny(frame, 80, 200)
+
+    all_images = {"Frame": frame, "Masks": masks, "Results": results, "Colors": color_filters, "Edges": edges, "Median Blur": medians, "Smooth Blur": smoothed }
+
+    if show_images:
+        display_images(all_images=all_images)
+    return all_images
+
+
+def display_images(frame=None, masks = None, edge=None, results = None, medians = None, smoothed = None,  all_images=None):
+    
+    if all_images is not None:
+        for image in all_images.keys():
+            if isinstance(all_images[image], dict):
+                for subimage in all_images[image].keys():
+                    cv2.imshow('{} {}'.format(subimage, image), all_images[image][subimage])
+            elif isinstance(all_images[image], list):
+                for i in range(len(all_images[image])-1):
+                    cv2.imshow('{}'.format(image), all_images[image][i])
+            elif all_images[image] is None:
+                pass
+            else:
+                cv2.imshow('{}'.format(image), all_images[image])
+    else:
+        if frame is None:
+            cv2.imshow('Frame', frame)
+        if masks is not None:
+            for key in masks.keys():
+                cv2.imshow('{} Mask'.format(key), masks[key])
+        
+        if results is not None:
+            for key in results.keys():
+                cv2.imshow('{} Result'.format(key), results[key])
+        
+        if medians is not None:
+            for key in medians.keys():
+                cv2.imshow('{} Median'.format(key), medians[key])
+        
+        if smoothed is not None:
+            for key in smoothed.keys():
+                cv2.imshow('{} smoothed'.format(key), smoothed[key])
+        if edge is not None:
+            cv2.imshow('Edges', edge)
+    return True
 
 def main():
     camera = 0
@@ -61,40 +130,7 @@ def main():
 
     while not finished:
         _, frame = cap.read()
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        fd = filters()
-        lowers = fd["Lower"]
-        uppers = fd["Upper"]
-        color_filters = fd["Filters"]
-
-        masks = {}
-        results = {}
-        medians = {}
-        smoothed = {}
-        kernel = np.ones((15,15), np.float32) / 225
-
-        #color_filters = sorted(lowers.keys())
-        for color in color_filters:
-            #print color
-            lower_color = np.array(lowers[color])
-            upper_color = np.array(uppers[color])
-            masks[color] = cv2.inRange(hsv, lower_color, upper_color)
-            results[color] = cv2.bitwise_and(frame, frame, mask=masks[color])
-            medians[color] = cv2.medianBlur(results[color], 15)
-            smoothed[color] = cv2.filter2D(results[color], -1, kernel)
-
-
-        edges = cv2.Canny(frame, 80, 200)
-
-        cv2.imshow('Frame', frame)
-        for color in masks.keys():
-            #cv2.imshow('{} Mask'.format(color), masks[color])
-            cv2.imshow('{} Result'.format(color), results[color])
-            cv2.imshow('{} Median'.format(color), medians[color])
-            cv2.imshow('{} smoothed'.format(color), smoothed[color])
-        #cv2.imshow('Edges', edges)
+        filter_colors(frame=frame, show_images=True, verbose=False)
 
         k = cv2.waitKey(5) & 0xFF
         if k == 27:
