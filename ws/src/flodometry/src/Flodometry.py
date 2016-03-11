560f0a8fff4e75d13a9d8574fbd3d0eb91077c91
Loren <Loren.sarthur@gmail.com>
2016-03-10 20:53:00 -0800

    Updated tests to use rosbag data, update kf.H measurement transform to more accurately move from a state to measurement frame.

--- ws/src/flodometry/src/Flodometry.py
+++ ws/src/flodometry/src/Flodometry.py
@@ -1,21 +1,35 @@
 #!/usr/bin/env python
 
-import rospy
+
 import math
 import numpy as np
+from bunch import Bunch
+CLOCK = 4*10**6
 
 # Bypass imports when runnning from test suite.
 if __name__ == '__main__':
+    import rospy
     from flodometry.msg import motion_read
     from nav_msgs.msg import Odometry
 else:
     motion_read = object()
-    Odometry = object()
+    odom = Bunch()
+    odom.header = Bunch()
+    odom.pose = Bunch()
+    odom.pose.pose = Bunch()
+    Odometry = lambda: odom
     class Rospy(object):
+        def __init__(self):
+            self.Time = Bunch()
+            self.Time.now = lambda: 1234
+
         def Subscriber(self, *args, **kwargs):
-            return object()
+            return Bunch()
         def Publisher(self, *args, **kwargs):
-            return object()
+            pub = Bunch()
+            pub.publish = lambda arg: True
+            return pub
+
     rospy = Rospy()
 
     
@@ -42,32 +56,37 @@ class Flodometry(object):
                         [0,  1]])  
         # Initialize the measurment transform with beta value from
         # file:///home/loren/Downloads/CTS12-37%20(1).pdf
-        self.kf.H = np.array([[0.0, (1.0/0.035)]])
+        self.kf.H = np.array([[0.0, 12188.48]])
         # Set initial variance to 0 since state is known
         self.kf.P *= 0
         # Initialize process noise with variance of max vel and white noise and dt of 0.1
-        self.kf.Q = Q_discrete_white_noise(dim=2, dt=0.1, var=0.2)
+        self.kf.Q = Q_discrete_white_noise(dim=2, dt=0.1, var=2)
         
 
     def update(self, motion):
         # Sensor was mounted so it only moved in the x direction during testing. 
-        vel = motion.dx
+        dt = float(motion.shutter)/CLOCK
+        vel = (motion.dx/dt)
+        var = abs(vel - motion.dx/(dt-1.0/16.0))
         # Set measurment accuaracy to 0.1 speed per 
         # file:///home/loren/Downloads/CTS12-37%20(1).pdf
-        self.kf.R = np.array([[abs(0.1*vel)]])
+        self.kf.R = np.array([[var]])
         self.kf.predict()
         self.kf.update(vel)
         self.publish_updates(self.kf.x, self.kf.P)
 
     def publish_updates(self, x, p):
-        odom = Odometry()
-        odom.header.stamp = rospy.Time.now()
-        odom.header.frame_id = '/odom'
-        odom.pose.pose.position.x = x[0]
-        odom.pose.covariance[0] = p[0][0]
-        odom.twist.twist.linear.x = x[1]
-        odom.twist.covariance[0] = p[1][1]
-        self.pub.publish(odom)
+        if __name__ == '__main__':
+            odom = Odometry()
+            odom.header.stamp = rospy.Time.now()
+            odom.header.frame_id = '/odom'
+            odom.pose.pose.position.x = x[0]
+            odom.pose.covariance[0] = p[0][0]
+            odom.twist.twist.linear.x = x[1]
+            odom.twist.covariance[0] = p[1][1]
+            self.pub.publish(odom)
+        else:
+            pass
 
 if __name__ == '__main__':
     rospy.init_node('flodometry')
--- ws/src/flodometry/test/test_flodometry.py
+++ ws/src/flodometry/test/test_flodometry.py
@@ -10,7 +10,19 @@ from src.Flodometry import Flodometry
 class TestFlodometry(unittest.TestCase):
     """TestFlodometry"""
 
-    def testUpdate(self):
+    def testUpdate_one(self):
+        self.f = Flodometry()
+        bag = rosbag.Bag('2016-03-09-21-01-43.bag')
+        for topic, msg, t in bag.read_messages(topics=['/optical_flow']):
+            try:
+                self.f.update(msg)
+            except Exception as ex:
+                import IPython; IPython.embed()
+        bag.close()
+        print self.f.kf.x
+        self.assertEquals(True, abs(abs(self.f.kf.x[0])-10) < 1)
+
+    def testUpdate_two(self):
         self.f = Flodometry()
         bag = rosbag.Bag('2016-03-09-21-03-00.bag')
         for topic, msg, t in bag.read_messages(topics=['/optical_flow']):
@@ -19,5 +31,18 @@ class TestFlodometry(unittest.TestCase):
             except Exception as ex:
                 import IPython; IPython.embed()
         bag.close()
-        import IPython; IPython.embed()
+        print self.f.kf.x
+        self.assertEquals(True, abs(abs(self.f.kf.x[0])-10) < 1)
+
+    def testUpdate_three(self):
+        self.f = Flodometry()
+        bag = rosbag.Bag('2016-03-09-21-07-21.bag')
+        for topic, msg, t in bag.read_messages(topics=['/optical_flow']):
+            try:
+                self.f.update(msg)
+            except Exception as ex:
+                import IPython; IPython.embed()
+        bag.close()
+        print self.f.kf.x
+        self.assertEquals(True, abs(abs(self.f.kf.x[0])-10) < 1)
         
