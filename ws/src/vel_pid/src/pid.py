3d35db01d4a893d06c4a745a0e3583b5a51a3ab8
Loren <Loren.sarthur@gmail.com>
2016-04-06 15:13:48 -0700

    Vel_PID tuning

--- ws/src/vel_pid/src/pid.py
+++ ws/src/vel_pid/src/pid.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import rospy
+import rospy, rosbag
 import time
 
 from uniboard_communication.srv import *
@@ -8,6 +8,7 @@ STEP_LIMIT = 0.01
 
 class PID(object):
     def __init__(self, outFun, kP, kI, kD, inRange, outRange, name):
+        self.name = name
         self.pub = rospy.Publisher(name+'_status', pid_status, queue_size=10)
         self.outFun = outFun
         self.kP = kP
@@ -19,31 +20,42 @@ class PID(object):
         self.err = []
         self.target = 0
         self.out = 0
+        self.stopped = True
+
+    def start(self):
+        self.stopped = False
+
+    def stop(self):
+        self.stopped = True
 
 
     def update(self, current):
-        err = self.target - current
-        self.err.append(float(err))
-        P = err*self.kP
-        x = len(self.err)
-        if x > 1:
-            i = sum(self.err)
-            d = self.err[x-1]-self.err[x-2]
+        if not self.stopped:
+            err = self.target - current
+            self.err.append(float(err))
+            P = err*self.kP
+            x = len(self.err)
+            if x > 1:
+                i = sum(self.err)
+                d = self.err[x-1]-self.err[x-2]
+            else:
+                i = 0
+                d = 0
+            I = self.kI*i
+            D = self.kD*d
+            out = P+I+D
+            # Scaling output to out range
+            # out = u*(float(self.outRange[1])/self.inRange[1])
+            if out > self.outRange[1]:
+                out = self.outRange[1]
+            elif out < self.outRange[0]:
+                out = self.outRange[0]
+            self.out = out
+            self.outFun(out)
+            self.publish_status(current, [P, I, D])
         else:
-            i = 0
-            d = 0
-        I = self.kI*i
-        D = self.kD*d
-        out = P+I+D
-        # Scaling output to out range
-        # out = u*(float(self.outRange[1])/self.inRange[1])
-        if out > self.outRange[1]:
-            out = self.outRange[1]
-        elif out < self.outRange[0]:
-            out = self.self.outRange[0]
-        self.out = out
-        self.outFun(out)
-        self.publish_status(current, [P, I, D])
+            self.out = 0
+            self.publish_status(current, [0,0,0])
 
 
     def integrate(self, f, x):
@@ -55,21 +67,39 @@ class PID(object):
 
     def publish_status(self, curr, pid_terms):
         status = pid_status()
-        status.status = "Running"
+        if self.stopped:
+            status.status = "Stopped"
+        else:
+            status.status = "Running"
         status.out = self.out
         status.target = self.target
         status.current = curr
-        self.pid_terms = pid_terms
+        status.pid_terms = pid_terms
         self.pub.publish(status)
 
 
     def set_target(self, target):
-        if target.target <= self.outRange[1] and target.target >= self.outRange[0]:
-            if self.target == target.target:
+        self.start()
+        rospy.logwarn(target)
+        if target <= self.outRange[1] and target >= self.outRange[0]:
+            if self.target == target:
                 return True
             else:
+                self.target = target
                 self.err = []
                 return True
         else:
             return False
             rospy.logwarn('Target set is out of input range')
+
+    def record(self):
+        self.bag = rosbag.Bag(self.name+'_'+str(rospy.Time.now())+'_recording.bag', 'w')
+        rospy.on_shutdown(self.stop_record)
+        rospy.Subscriber(self.name+'_status', pid_status, self._record)
+
+    def _record(self, status):
+        #self.bag.write(status, pid_status)
+        rospy.loginfo(status)
+
+    def stop_record(self):
+        self.bag.close()
