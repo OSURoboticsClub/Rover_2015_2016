d3d85665681d0c18fa4154ded16a82f1c1134534
Jay Fenton <jfenton6134@gmail.com>
2016-04-25 20:29:30 -0700

    reliable and tested pick up function in vid.py

--- ws/src/pkg/nodes/vid.py
+++ ws/src/pkg/nodes/vid.py
@@ -13,7 +13,7 @@ import cv2
 
 new_crotch_img2 = None
 bridge = CvBridge()
-cv2.namedWindow('DICKBUTT')
+#cv2.namedWindow('DICKBUTT')
 
 TEST_PIT = True
 
@@ -25,29 +25,6 @@ def handle_img(img):
     except CvBridgeError as e:
         rospy.logerror(e)
 
-def pick_up_center(u):
-
-    u.arm_target("Y", u.arm_max("Y")/2) # move to center
-    u.arm_target("X", u.arm_max("X")/2) # move to center
-
-    while u.arm_should_be_moving("X") or u.arm_should_be_moving("Y"): pass
-
-    z_safe_move(u, 0.2)
-    u.arm_z_wait_until_done()
-
-    u.arm_target("A", 0.99)
-    #rospy.loginfo("should be opening")
-    time.sleep(5)
-
-    z_safe_move(u, 0)
-    u.arm_z_wait_until_done()
-
-    u.arm_target("A", 0)
-    time.sleep(5)
-
-    z_safe_move(u, 0.5)
-    u.arm_z_wait_until_done()
-
 def z_safe_move(u, z):
 	"""Z values from .5 to 0, with .5 being upright."""
 	z = .5 - z
@@ -56,15 +33,46 @@ def z_safe_move(u, z):
 	else:
 		u.arm_target("Z", .5-z)
 
+def up_down_test(u):
+    
+    u.arm_target("X", 0.25)
+    while u.arm_should_be_moving("X"): pass
+
+    for i in range(5):
+       z_safe_move(u, 0.03)
+       u.arm_z_wait_until_done()
+       z_safe_move(u, 0.5)
+       u.arm_z_wait_until_done()
+    
+    '''
+    u.arm_target("X", 0.1)
+    while u.arm_should_be_moving("X"): pass
+
+    for i in range(5):
+       z_safe_move(u, 0.03)
+       u.arm_z_wait_until_done()
+       z_safe_move(u, 0.5)
+       u.arm_z_wait_until_done()
+ 
+    u.arm_target("X", 0.15)
+    while u.arm_should_be_moving("X"): pass
+
+    for i in range(5):
+       z_safe_move(u, 0.03)
+       u.arm_z_wait_until_done()
+       z_safe_move(u, 0.5)
+       u.arm_z_wait_until_done()
+    '''
+
 def pick_up_at(u,xy):
 
     # constants that define the range of the arm
     # from the pit cam's perspective
     # TODO: measure these
-    X_CAM_MAX = 800.0
-    X_CAM_MIN = 439.0
-    Y_CAM_MAX = 425.0
-    Y_CAM_MIN = 61.0
+    X_CAM_MAX = 870.0
+    X_CAM_MIN = 435.0
+    Y_CAM_MAX = 383.0#128.0
+    Y_CAM_MIN = 119.0#354.0
 
     REAL_X = 0.29
     REAL_Y = 0.24
@@ -74,8 +82,8 @@ def pick_up_at(u,xy):
     
     # assumes camera is oriented so that (0,0)
     # of the camera corresponds to (0,0) of the arm
-    if xy[0] > X_CAM_MAX: rospy.loginfo("the sample is too far right to pick up")
-    elif xy[0] < X_CAM_MIN: rospy.loginfo("the sample is too far left to pick up")
+    if xy[0] > X_CAM_MAX: rospy.loginfo("the sample is too far left to pick up")
+    elif xy[0] < X_CAM_MIN: rospy.loginfo("the sample is too far right to pick up")
     elif xy[1] > Y_CAM_MAX: rospy.loginfo("the sample is too far forward to pick up")
     elif xy[1] < Y_CAM_MIN: rospy.loginfo("the sample is too far backward to pick up")
     else:
@@ -87,24 +95,28 @@ def pick_up_at(u,xy):
         x_pick = (x_sample/ACTUAL_CAM_LENGTH_X) * u.arm_max("X")
         y_pick = (y_sample/ACTUAL_CAM_LENGTH_Y) * u.arm_max("Y")
 
+        x_pick += 0.0
+        y_pick -= 0.125
+
         u.arm_target("X", x_pick)
         u.arm_target("Y", y_pick)
 
         rospy.loginfo("calculated x: " + str(x_pick))
         rospy.loginfo("calculated y: " + str(y_pick))
 
-        #x_pick += 0.07
-        x_pick += 0.00
-        y_pick -= 0.00
-
         while u.arm_should_be_moving("X") or u.arm_should_be_moving("Y"): pass
 
         z_safe_move(u, 0.2)
         u.arm_z_wait_until_done()
 
+        u.arm_target("A", .05)
+	while u.arm_should_be_moving("A"):
+	    time.sleep(.1)
+	    u.arm_go("A", True)
+
         u.arm_target("A", 0.99)
-        time.sleep(5) 
-	z_safe_move(u, 0.047)
+        time.sleep(3) 
+	z_safe_move(u, 0.035)
         u.arm_z_wait_until_done()
 
         #u.arm_target("A", 0.5)
@@ -118,23 +130,6 @@ def pick_up_at(u,xy):
 
         z_safe_move(u, 0.5)
         u.arm_z_wait_until_done()
-
-def drop(u):
-
-   z_safe_move(u, 0)
-   u.arm_z_wait_until_done()
-
-   u.arm_target("A", 1)
-   time.sleep(5)
-
-   z_safe_move(u, 0.2)
-   u.arm_z_wait_until_done()
-
-   u.arm_target("A", 0)
-   time.sleep(5)
-
-   z_safe_move(u, 0.5)
-   u.arm_z_wait_until_done()
 
 def vid():
     rospy.init_node('vid', anonymous=False)
@@ -150,7 +145,7 @@ def vid():
     #time.sleep(9)
     #u.motor_left(0)
     #u.motor_right(0)
-
+    
     # PICK UP SAMPLE
     if TEST_PIT:
        # only used for pit camera tests
@@ -162,7 +157,7 @@ def vid():
        if new_crotch_img2 is not None:
           curr_crotch_img = new_crotch_img2
           rospy.loginfo("image success")
-          cv2.imshow('DICKBUTT', curr_crotch_img)
+          #cv2.imshow('DICKBUTT', curr_crotch_img)
           xy = grab.identify_easy_sample(curr_crotch_img)
           rospy.loginfo("coordinates: " + str(xy))
           if xy:
@@ -182,6 +177,8 @@ def vid():
 
     # DROP SAMPLE
     # drop(u)
+
+    #up_down_test(u)
 
 if __name__ == '__main__':
     try:
