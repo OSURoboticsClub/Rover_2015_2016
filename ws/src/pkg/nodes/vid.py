50a09bbb14dd5f90f8816e8a800df8265bd8ca56
TimRice1994 <timrice1994@gmail.com>
2016-04-26 15:16:26 -0700

    removing duplicate code

--- ws/src/pkg/nodes/vid.py
+++ ws/src/pkg/nodes/vid.py
@@ -24,73 +24,6 @@ def handle_img(img):
     except CvBridgeError as e:
         rospy.logerror(e)
 
-def z_safe_move(u, z):
-	"""Z values from .5 to 0, with .5 being upright."""
-	z = .5 - z
-	if u.arm_current("X", 0) > u.arm_max("X")/2:
-        	u.arm_target("Z", .5+z)
-	else:
-		u.arm_target("Z", .5-z)
-
-def pick_up_at(u,xy):
-
-    # constants that define the range of the arm
-    # from the pit cam's perspective
-    X_CAM_MAX = 870.0
-    X_CAM_MIN = 435.0
-    Y_CAM_MAX = 383.0
-    Y_CAM_MIN = 119.0
-
-    REAL_X = 0.29
-    REAL_Y = 0.24
-
-    ACTUAL_CAM_LENGTH_X = X_CAM_MAX-X_CAM_MIN
-    ACTUAL_CAM_LENGTH_Y = Y_CAM_MAX-Y_CAM_MIN
-    
-    # assumes camera is oriented so that (0,0)
-    # of the camera corresponds to (0,0) of the arm
-    if xy[0] > X_CAM_MAX: rospy.loginfo("the sample is too far left to pick up")
-    elif xy[0] < X_CAM_MIN: rospy.loginfo("the sample is too far right to pick up")
-    elif xy[1] > Y_CAM_MAX: rospy.loginfo("the sample is too far forward to pick up")
-    elif xy[1] < Y_CAM_MIN: rospy.loginfo("the sample is too far backward to pick up")
-    else:
-        x_sample = xy[0]-X_CAM_MIN
-        y_sample = Y_CAM_MAX-(xy[1]-Y_CAM_MIN)
-
-        # convert to meters
-        # x_pick/x_arm_max = x_sample/x_cam_max
-        x_pick = (x_sample/ACTUAL_CAM_LENGTH_X) * u.arm_max("X")
-        y_pick = (y_sample/ACTUAL_CAM_LENGTH_Y) * u.arm_max("Y")
-
-        x_pick += 0.0
-        y_pick -= 0.125
-
-        u.arm_target("X", x_pick)
-        u.arm_target("Y", y_pick)
-
-        rospy.loginfo("calculated x: " + str(x_pick))
-        rospy.loginfo("calculated y: " + str(y_pick))
-
-        while u.arm_should_be_moving("X") or u.arm_should_be_moving("Y"): pass
-
-        z_safe_move(u, 0.2)
-        u.arm_z_wait_until_done()
-
-        u.arm_target("A", .05)
-	while u.arm_should_be_moving("A"):
-	    time.sleep(.1)
-	    u.arm_go("A", True)
-
-        u.arm_target("A", 0.99)
-        time.sleep(3) 
-	z_safe_move(u, 0.04)
-        u.arm_z_wait_until_done()
-
-        u.arm_target("A", 0)
-        time.sleep(3)
-
-        z_safe_move(u, 0.5)
-        u.arm_z_wait_until_done()
 
 def vid():
     rospy.init_node('vid', anonymous=False)
@@ -121,7 +54,7 @@ def vid():
           rospy.loginfo("coordinates: " + str(xy))
           if xy:
              rospy.loginfo("a sample has been detected")
-             pick_up_at(u,xy)
+             grab.pick_up_at(u,xy)
           else:
              rospy.loginfo("no sample detected")
     else:
