a2beaae8cdbb51dedc54b48e9ec9006d85b9b0fc
Jay Fenton <jfenton6134@gmail.com>
2016-04-27 17:52:53 -0700

    adding map and odom frame transforms - currently do no transformation

--- ws/src/transforms/src/base_broadcaster.cpp
+++ ws/src/transforms/src/base_broadcaster.cpp
@@ -7,18 +7,30 @@ int main(int argc, char** argv){
 
 	ros::Rate r(100);
 
- 	tf::TransformBroadcaster broadcaster;
+ 	tf::TransformBroadcaster base_broadcaster;
+	tf::TransformBroadcaster odom_link;
+	tf::TransformBroadcaster map_link;
 
   	while(n.ok()){
 	// tf::Quat represents rotation and we have none
 	// tf::Vect represents the translation corresponding to the zed offset from the rovers base in meters
 	// timestamp is set to now
 	// parent node created is base link, child node camera_link
-   		broadcaster.sendTransform(
+   		base_broadcaster.sendTransform(
  		 tf::StampedTransform(
 		  tf::Transform(tf::Quaternion( 0, -1, 0, 1), tf::Vector3(0.1, 0.0, 1.0)), 	
 		  ros::Time::now(),"base_link", "camera_link"));
-    		
+    		//odom to base
+		odom_link.sendTransform(
+		 tf::StampedTransform(
+		  tf::Transform(tf::Quaternion( 0, 0, 0, 1), tf::Vector3(0.0, 0.0, 0.0)), 	
+		  ros::Time::now(),"odom", "base_link"));
+		r.sleep();
+		//map to odom
+		map_link.sendTransform(
+		 tf::StampedTransform(
+		  tf::Transform(tf::Quaternion( 0, 0, 0, 1), tf::Vector3(0.0, 0.0, 0.0)),
+		  ros::Time::now(),"map", "odom"));
 		r.sleep();
   	}
 }
