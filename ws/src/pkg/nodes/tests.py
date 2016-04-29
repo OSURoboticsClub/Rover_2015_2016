294893acd1fcfb2edacf450b6bd03ba9853edf23
TimRice1994 <timrice1994@gmail.com>
2016-04-28 19:23:58 -0700

    adding precached sample tests

--- ws/src/pkg/nodes/tests.py
+++ ws/src/pkg/nodes/tests.py
@@ -40,7 +40,7 @@ def handle_img(img):
     except CvBridgeError as e:
         rospy.logerror(e)
 
-def turn_to_sample(u, coords):
+def turn_to_sample(u, coords, precached):
     print "turning" 
     rospy.loginfo("COORDS[0]: " + str(coords[0]))
     while coords[0] > 40 or coords[0] < -40:
@@ -51,7 +51,10 @@ def turn_to_sample(u, coords):
           time.sleep(1)
           u.motor_left(0.0)
           u.motor_right(0.0)
-          coords = scan.check_easy_sample(scan_img_left, scan_img_right)
+          if precached:
+              coords = scan.check_precached(scan_img_left, scan_img_right)
+          else:
+              coords = scan.check_easy_sample(scan_img_left, scan_img_right)
        elif coords[0] <= 0:
           rospy.loginfo("turn left")
           u.motor_left(-0.22)
@@ -59,9 +62,15 @@ def turn_to_sample(u, coords):
           time.sleep(1)
           u.motor_right(0.0)
           u.motor_left(0.0)
-          coords = scan.check_easy_sample(scan_img_left, scan_img_right)
+          if precached:
+              coords = scan.check_precached(scan_img_left, scan_img_right)
+          else:
+              coords = scan.check_easy_sample(scan_img_left, scan_img_right)
        while coords is None:
-          coords = scan.check_easy_sample(scan_img_left, scan_img_right)
+          if precached:
+              coords = scan.check_precached(scan_img_left, scan_img_right)
+          else:
+              coords = scan.check_easy_sample(scan_img_left, scan_img_right)
 
 # moves forward until scan cam sees sample,
 # then stops and returns coordinates
@@ -133,13 +142,19 @@ def test_pickup_easy(u):
        else:
           rospy.loginfo("no sample detected")
 
+def test_pickup_precached(u):
+    
+    u.motor_left(0.1)
+    u.motor_right(0.1)
+    time.sleep(3)
+
 def test_scan_and_grab_easy(u):
    
     # move forward until scan cam sees sample 
     coords = test_forward_until_scanned(u, False)
 
     # turn so that the sample is in front of the rover
-    turn_to_sample(u, coords)
+    turn_to_sample(u, coords, False)
     #print "not turning"
 
     # move forward until the sample is seen by the pit cam, and park over it
@@ -148,13 +163,22 @@ def test_scan_and_grab_easy(u):
     # pick up sample
     test_pickup_easy(u)
 
+def test_scan_and_grab_precached(u):
+    coords = test_forward_until_scanned(u, True)
+
+    turn_to_sample(u, coords, True)
+
+    test_move_til_sample(u, True)
+
+    test_pickup_precached(u)
+
 def tests():
     rospy.init_node('tests', anonymous=False)
     rospy.loginfo('node initialized')    
     rospy.Subscriber('crotch/image/image_raw', Image, handle_img)
     rospy.Subscriber('stereo/left/image_rect_color', Image, handle_scan_left)
     rospy.Subscriber('stereo/right/image_rect_color', Image, handle_scan_right)
-    rate = rospy.Rate(10)
+    #rate = rospy.Rate(10)
     u = uniboard.Uniboard("/dev/ttyUSB1")
     u.arm_home()
     
@@ -165,9 +189,8 @@ def tests():
     #test_pickup_easy(u)
     test_scan_and_grab_easy(u)
 
-    # TODO: 
-    # test_pickup_precached?
-    # test_scan_and_grab_precached
+    #test_pickup_precached(u)
+    #test_scan_and_grab_precached(u)
 
 if __name__ == '__main__':
     try:
