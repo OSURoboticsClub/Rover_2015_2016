e27b4f6b578bde5bdd274e96047f68ff52172eff
Jay Fenton <jfenton6134@gmail.com>
2016-05-03 17:28:54 -0700

    Updating rover_2dnav launch files and params, local costmap now functions

--- ws/src/transforms/src/base_broadcaster.cpp
+++ ws/src/transforms/src/base_broadcaster.cpp
@@ -10,20 +10,20 @@ int main(int argc, char** argv){
  	tf::TransformBroadcaster base_broadcaster;
 	//tf::TransformBroadcaster odom_link;
 	tf::TransformBroadcaster map_link;
-	//tf::Transform transform;
+	tf::Transform transform;
 
   	while(n.ok()){
-	// tf::Quat represents rotation and we have none
+	// tf::Quat represents rotation 
 	// tf::Vect represents the translation corresponding to the zed offset from the rovers base in meters
 	// timestamp is set to now
 	// parent node created is base link, child node camera_link
-	transform.setOrigin( tf::Vector3(0.43, 0.0, 0.86) );
-        transform.setRotation( tf::Quaternion(0, -1, 0, 1) );
-        base_broadcaster.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_link", "camera_link"));
+	transform.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
+        transform.setRotation( tf::Quaternion(1, -1, 1, 1) );
+       	base_broadcaster.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_link", "camera_link"));
 
-	/*transform.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
+/*	transform.setOrigin( tf::Vector3(3.0, 3.0, 0.0) );
         transform.setRotation( tf::Quaternion(0, 0, 0, 1) );
-        odom_link.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "odom", "base_link"));*/
+        odom_link.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "odom", "base_link")); */
 
 	transform.setOrigin( tf::Vector3(3.0, 3.0, 0.0) );
         transform.setRotation( tf::Quaternion(0, 0, 0, 1) );
