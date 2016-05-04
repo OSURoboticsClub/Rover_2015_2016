376a35b8841474e857055e717148405f3c4f30ec
Jay Fenton <jfenton6134@gmail.com>
2016-05-04 15:34:24 -0700

    got movement visualized in RVIZ with lorens bag files

--- ws/src/simple_navigation_goals/src/simple_goals.cpp
+++ ws/src/simple_navigation_goals/src/simple_goals.cpp
@@ -5,34 +5,35 @@
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 int main(int argc, char** argv){
-  ros::init(argc, argv, "simple_navigation_goals");
+	ros::init(argc, argv, "simple_navigation_goals");
 
-  //tell the action client that we want to spin a thread by default
-  MoveBaseClient ac("move_base", true);
+	//tell the action client that we want to spin a thread by default
+	MoveBaseClient ac("move_base", true);
 
-  //wait for the action server to come up
-  while(!ac.waitForServer(ros::Duration(5.0))){
-    ROS_INFO("Waiting for the move_base action server to come up");
-  }
+	//wait for the action server to come up
+ 	while(!ac.waitForServer(ros::Duration(5.0))){
+		ROS_INFO("Waiting for the move_base action server to come up");
+	}
 
-  move_base_msgs::MoveBaseGoal goal;
+	move_base_msgs::MoveBaseGoal goal;
 
-  //we'll send a goal to the robot to move 1 meter forward
-  goal.target_pose.header.frame_id = "base_link";
-  goal.target_pose.header.stamp = ros::Time::now();
+  	goal.target_pose.header.frame_id = "map";
+  	goal.target_pose.header.stamp = ros::Time::now();
+	
+	// current goals are meant to be used with bag_four
+  	goal.target_pose.pose.position.x = 13.9;
+	goal.target_pose.pose.position.y = 2.8;
+  	goal.target_pose.pose.orientation.w = 1.0;
 
-  goal.target_pose.pose.position.x = 1.0;
-  goal.target_pose.pose.orientation.w = 1.0;
+  	ROS_INFO("Sending goal");
+  	ac.sendGoal(goal);
 
-  ROS_INFO("Sending goal");
-  ac.sendGoal(goal);
+  	ac.waitForResult();
 
-  ac.waitForResult();
+  	if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+   	 	ROS_INFO("Goal met!");
+  	else
+   		ROS_INFO("Goal failed");
 
-  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Hooray, the base moved 1 meter forward");
-  else
-    ROS_INFO("The base failed to move forward 1 meter for some reason");
-
-  return 0;
+	return 0;
 }
