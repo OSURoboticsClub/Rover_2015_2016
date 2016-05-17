793a4b9cb36060eed85a615d83654e47c32511f5
Jay Fenton <jfenton6134@gmail.com>
2016-05-17 16:49:43 -0700

    Updated simple goals to communicate over service a refactor of functions

--- ws/src/simple_navigation_goals/src/simple_goals.cpp
+++ ws/src/simple_navigation_goals/src/simple_goals.cpp
@@ -2,6 +2,11 @@
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 #include <tf/transform_listener.h>
+#include "locate_home_base/locate_base.h"
+#include "../../locate_home_base/src/HomeBase.h"
+#include <cmath>
+
+#define PI 3.14159265
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
@@ -10,6 +15,9 @@ struct pos2d
 	float x;
 	float y;
 };
+
+// this array will define the planned stops for the rover over level 1
+pos2d level1Stops[3] = {{6,3}, {6,12}, {1,1}};
 
 /* create goal takes (x,y) position and a z angle
 Returns: MoveBaseGoal
@@ -29,6 +37,42 @@ move_base_msgs::MoveBaseGoal createGoal(struct pos2d pos, float z)
   	goal.target_pose.pose.orientation.w = z;
 
 	return goal;
+}
+
+/* executeGoal takes a goal, pointer to a client, and a timeout value and runs the goal
+timeout value should be longer than the time to achieve the goal
+goal can be created with a goal to createGoal
+*/
+bool executeGoal(move_base_msgs::MoveBaseGoal goal, MoveBaseClient *client, int timeout)
+{
+	ROS_INFO("Sending goal");
+  	client->sendGoal(goal);
+
+	// wait for result or time out after 60s 
+  	bool finished_before_timeout = client->waitForResult(ros::Duration(timeout));
+	
+	// check goal outcome
+	if (finished_before_timeout)
+  	{
+		if(client->getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+		{
+			ROS_INFO("MOVE BASE: goal met");
+			//ros::param::set("/base_goal_param", true);
+			return(true);
+		}
+  		else
+		{
+			ROS_INFO("MOVE BASE: missed goal");
+			//ros::param::set("/base_goal_param", false);
+			return(false);
+		}
+  	}
+  	else
+	{
+		ROS_INFO("MOVE BASE: time out");
+		//ros::param::set("/base_goal_param", false);
+		return(false);
+	}
 }
 
 /* getCurrentPos logs and returns current x,y position of the robot in the map frame
@@ -60,48 +104,78 @@ struct pos2d getCurrentPos(void)
     	} 
 }
 
+struct pos2d getCurrentPosFromCheckboard(ros::ServiceClient client)
+{
+	float base_distance, base_angle;
+	locate_home_base::locate_base srv;
+	struct pos2d coord;
+
+	srv.request.header.stamp = ros::Time::now();
+
+	if (client.call(srv))
+	{
+		base_distance = (float)srv.response.distance;
+		base_angle = (float)srv.response.angle;
+
+		if (base_distance == CANT_FIND)
+		{ 
+			ROS_ERROR("Couldn't see checkerboard");
+			coord.x = -1;
+			coord.y = -1;
+		}
+		else
+		{	
+			// convert angle and distance to x,y from base station
+			ROS_INFO("Checkerboard detected");	
+			coord.x =  base_distance*cos(abs(base_angle) * PI / 180.0)/1000;		
+			if (base_angle < 0)
+				coord.y =  -base_distance*sin(abs(base_angle) * PI / 180.0)/1000;
+			else 
+				coord.y =  base_distance*sin(abs(base_angle) * PI / 180.0)/1000;
+			ROS_INFO_STREAM("X: "<<posEnd.x<<" Y: "<<posEnd.y);	
+		}
+  	}
+  	else
+  	{
+    		ROS_ERROR("Failed to call service locate_base");
+		coord.x = -1;
+		coord.y = -1;
+  	}
+	
+	return coord;
+}
+
 int main(int argc, char** argv){
 	ros::init(argc, argv, "simple_navigation_goals");
 
-	// node handle to listen in on transforms
-	ros::NodeHandle n;
+	// node handle to communication with service from locate base
+	ros::NodeHandle nsrv; 
+	ros::ServiceClient client = nsrv.serviceClient<locate_home_base::locate_base>("locate_base");
+
+	// base goal param can be queried by nodes to pass control
+	// false is set until a goal is met
+	ros::param::set("/base_goal_param", false);
 
 	//tell the action client that we want to spin a thread
 	MoveBaseClient ac("move_base", true);
 
 	move_base_msgs::MoveBaseGoal goal;
-	struct pos2d posSet, posEnd;
-
+	struct pos2d posEnd;
+	bool goalOutcome;
+	
 	//wait for the action server to come up
  	while(!ac.waitForServer(ros::Duration(5.0))){
 		ROS_INFO("Waiting for the move_base action server to come up");
 	}
-
-// GOAL (still set for bag 4 from old tests)
-	posSet.x = 7;
-	posSet.y = 0;
-
-	goal = createGoal(posSet, 1);
-
-  	ROS_INFO("Sending goal");
-  	ac.sendGoal(goal);
-
-	// wait for result or time out after 60s 
-  	bool finished_before_timeout = ac.waitForResult(ros::Duration(60.0));
 	
-	// get pos after goal met or time out
-	posEnd = getCurrentPos();
-	
-	if (finished_before_timeout)
-  	{
-    		//ROS_INFO("Action finished: %s",ac.getState.toString().c_str());
-		if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-   	 		ROS_INFO("Goal met!");
-  		else
-   			ROS_INFO("Goal failed");
-  	}
-  	else
-		ROS_INFO("Action did not finish before the time out.");
-	
+	for (int i = 0; i < sizeof(level1Stops)/sizeof(struct pos2d); i++)
+	{
+		goal = createGoal(level1Stops[i], 1);
+		goalOutcome = executeGoal(goal, &ac, 60);
+		//recovery
+		//transfer control
+	}
+
+	// look for checkboard and create final goal
 	return 0;
 }
